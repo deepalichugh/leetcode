8d14811fe40b1ce8f2eb69a14906b5200379f4a2
Deepali chugh <deepalichugh@deepalis-laptop.mynetworksettings.com>
2026-01-13 16:31:15 -0500

    Added Contains Duplicate Problem

---
+++ contains_duplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+    let hasSeen = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (hasSeen.has(nums[i])) {
+            return true;
+        }
+        hasSeen.add(nums[i]);
+    }
+
+    return false;
+};
