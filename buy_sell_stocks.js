139d601c46f59cbec8f4056f84b269bd1d8385bb
Deepali chugh <chugh@deepali.com>
2026-05-02 16:37:33 -0400

    Updated solution for lc problem 121

---
+++ buy_sell_stocks.js
@@ -0,0 +1,13 @@
+function maxProfit (prices) {
+    let left = 0;
+    let profit = 0;
+    for (let right = 1; right < prices.length; right++) {
+        if (prices[right] < prices[left]) {
+            left = right;
+        }
+
+        profit = Math.max(profit, prices[right] - prices[left]);
+    }
+
+    return profit;
+}
